52f573a7d790ad8b439d71db4fb4fc21b62b66c2
rinkuberde <rinkuberde@gmail.com>
2023-02-25 14:17:48 +0530

    Welcome Message

---
+++ AddressBookSystem-LINQ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookSystem_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To AddressBook System Using LINQ");
+        }
+    }
+}
